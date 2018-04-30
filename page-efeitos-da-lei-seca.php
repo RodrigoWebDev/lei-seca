432d24771fc15f798afb0650e10a29e83af65319
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-04-29 23:08:09 -0300

    Adicionado textos nas paginas EFEITOS DA LEI SECA e O QUE É A LEI SECA

---
+++ page-efeitos-da-lei-seca.php
@@ -0,0 +1,99 @@
+<?php get_header();?>
+
+<div class="nav-button hover">
+    <i class="ion-navicon-round"></i>
+</div>
+<!------------------ HERO IMAGE -------------------->
+
+<div class="efeitos-hero position-relative d-flex align-items-center justify-content-center">
+    <h1 class="d-none">Os efeitos da Lei seca</h1>
+    <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/efeitos-title.png'?>" alt="Os efeitos da Lei Seca">
+</div>
+
+
+<!------------------ EFEITOS CONTENT -------------------->
+<div class="container">
+    <section class="efeitos-content py-5">
+        <p>Desde 2008, mais de 20 mil ações de fiscalização foram realizadas por todo o Estado do Rio de Janeiro, abordando mais de 2 milhões de motoristas. Aproximadamente 183 mil pessoas dentre estes motoristas apresentavam sinais de embriaguez e tiveram suas carteiras de habilitação recolhidas. Além disso, foram multados mais de 520 mil veículos.</p>
+        <p>Perto de completar dez anos de sua implementação, a Lei Seca já salvou mais de 41 mil vidas, além de ter poupado ao país mais de R$ 550 bilhões por evitar perda de produtos e renda, de acordo com estudo realizado pelo Centro de Pesquisa e Economia do Seguro (CPES), da Escola Nacional de Seguros. </p>
+        <p>O mesmo estudo aponta que a versão de 2012 da Lei Seca obteve um impacto maior que a lei original, com uma redução de acidentes quase três vezes maior que a versão anterior da lei, graças às suas penas mais duras. Só no Rio de Janeiro, os acidentes e mortes no trânsito reduziram cerca de 34% e menos 43% motoristas flagrados alcoolizados. Internações e prestação de socorro por causa de acidentes de trânsito também geram altos custos sociais e também foi um número que teve diminuição: quase 50% de internações a menos.</p>
+    </section>
+</div>
+
+<!------------------ ESTATISTICAS -------------------->
+<section class="stats">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-2">
+                <br>
+                <h2>Vidas Salvas</h2>
+                <img class="img-fluid"src="<?php echo get_template_directory_uri() . '/img/stats1.png'?>" alt="Estatistica">
+            </div>
+            <div class="col-6 col-md-2">
+                <h2>Ações de<br> Fiscalização</h2>
+                <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/stats2.png'?>" alt="Estatistica">
+            </div>
+        
+            <div class="col-6 col-md-2">
+                <h2>Motoristas <br>Abordados</h2>
+                <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/stats3.png'?>" alt="Estatistica">
+            </div>
+            <div class="col-6 col-md-2">
+                <h2>Carteiras <br>recolhidas</h2>
+                <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/stats4.png'?>" alt="Estatistica">
+            </div>
+            <div class="col-6 col-md-2">
+                <h2>Veiculos <br> Multados</h2>
+                <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/stats1.png'?>" alt="Estatistica">
+            </div>
+        </div>
+    </div>
+</section>
+
+<!------------------ e mais... -------------------->
+<section class="e-mais pt-5">
+    <h2 class="text-center">e mais...</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/icone-grafico.png'?>" alt="Grafico de economia">
+                    </div>
+                    <div class="col-6 d-flex align-items-center">
+                        <p>Economia de R$550 milhões por evitar perda de produtos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/icone-coração.png'?>" alt="Grafico de economia">
+                    </div>
+                    <div class="col-6 d-flex align-items-center">
+                        <p>No Rio de Janeiro, 34% de redução de acidentes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/icone-hospital.png'?>" alt="Grafico de economia">
+                    </div>
+                    <div class="col-6 d-flex align-items-center">
+                        <p>Menos 50% de internações por acidentes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+            
+<?php get_footer(); ?>
+
+</body>
+</html>
--- page-o-que-e-a-lei-seca.php
+++ page-o-que-e-a-lei-seca.php
@@ -14,7 +14,7 @@
                 <div class="vertical-line d-none d-md-block"></div>
             </div>
             <div class="col-md-5 d-flex align-items-center">
-                <p class="p-4 pr-md-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit nam laborum non alias, reprehenderit doloremque labore. Voluptatem maxime nobis doloremque tenetur ullam similique pariatur, ipsa omnis impedit, beatae labore facilis!Atque praesentium repudiandae eligendi eaque harum? Nihil quo, eum fugit voluptas architecto, aliquam illum facere et voluptatibus dolores dolore temporibus nobis neque accusamus quisquam adipisci explicabo numquam animi ipsam rem.</p>
+                <p class="p-4 pr-md-5">A Lei nº 11.705, de 19 de junho de 2008, mais conhecida como Lei Seca, de autoria do Deputado Federal pelo Rio de Janeiro Hugo Leal, é uma política pública permanente que foi criada para ajudar na fiscalização de motoristas alcoolizados e aumentar a conscientização para os perigos de combinar bebida e direção. A lei vem da união de três órgãos, a Secretaria de Governo, o Detran e a Polícia Militar que além de de campanhas de conscientização, houve bastante dedicação para realizar blitz e verificar aqueles que infringiram a legislação.</p>
             </div>
         </div>
         <a href="#lei-seca"><i class="d-none d-md-block ion-chevron-down position-absolute hover"></i></a>
@@ -30,7 +30,7 @@
             <div class="col-md-6 p-md-5 position-relative">
                 <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2009-2.png'?>">
                 <h2 class="text-uppercase text-center">A primeira operação Lei Seca</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sint voluptatibus reprehenderit! Obcaecati, facilis fugit animi necessitatibus nostrum officiis architecto magnam ducimus impedit optio eveniet tempore quaerat eum cupiditate cumque!Ex ad est recusandae laudantium natus, repellendus placeat atque quis sint accusantium porro, dignissimos fuga voluptates aliquam in, sit distinctio id sed. Dolorem unde culpa ipsum ab neque earum quam.</p>
+                <p>A primeira Operação Lei Seca ocorreu em 19 de março de 2009, e, no seu início, 20% dos motoristas registravam a presença de bebida alcoólica no sangue. Em 2016, o número de motoristas pegos com traços de bebida alcoólica no sangue caiu para 7%. Já em seu primeiro mês de funcionamento, a lei mostrou que faz a diferença. Segundo dados do Ministério da Saúde, ocorreu no período uma diminuição de 24% de resgates do SAMU. Em um ano, o número de mortes em acidentes causados por motoristas embriagados caiu em 32% em comparação com o ano anterior.</p>
             </div>
         </div>
         <div class="row">
@@ -41,7 +41,7 @@
             <div class="col-md-6">
                 <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2012-2.png'?>">
                 <h2 class="text-uppercase text-center">Modificações da Lei Seca</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sint voluptatibus reprehenderit! Obcaecati, facilis fugit animi necessitatibus nostrum officiis architecto magnam ducimus impedit optio eveniet tempore quaerat eum cupiditate cumque!Ex ad est recusandae laudantium natus, repellendus placeat atque quis sint accusantium porro, dignissimos fuga voluptates aliquam in, sit distinctio id sed. Dolorem unde culpa ipsum ab neque earum quam.</p>
+                <p>A lei foi modificada em 2012, para endurecer ainda mais a fiscalização no trânsito. Inicialmente a prova da embriaguez só poderia ser feita através do teste do bafômetro ou exame do sangue do motorista em laboratório. Após a mudança, passaram a valer como provas o depoimento do policial, testemunhos de terceiros, fotos e vídeos, além de um aumento considerável no valor da multa para o condutor que for flagrado sob influência do álcool. Com essas modificações diminuíram ainda mais as ocorrências de motoristas alcoolizados e acidentes causados pela combinação de álcool e direção. </p>
             </div>
             
         </div>
@@ -64,7 +64,7 @@
             <div class="col-md-6">
                 <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2017-2.png'?>">
                 <h2 class="text-uppercase text-center">Trânsito mais humanizado</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sint voluptatibus reprehenderit! Obcaecati, facilis fugit animi necessitatibus nostrum officiis architecto magnam ducimus impedit optio eveniet tempore quaerat eum cupiditate cumque!Ex ad est recusandae laudantium natus, repellendus placeat atque quis sint accusantium porro, dignissimos fuga voluptates aliquam in, sit distinctio id sed. Dolorem unde culpa ipsum ab neque earum quam.</p>
+                <p>Em 2016, novas modificações foram feitas para aprimorar a Lei Seca e o valor da multa aplicada ao motorista que dirige embriagado aumentou para 10 no fator de multiplicação: De R$1.912,00 para R$2.934,70, além de ter habilitação suspensa pelo período de 12 meses. A intenção é que o trânsito seja humanizado, com a contribuição e união de todos. A reeducação é necessária e está sendo feita através das penalizações para reduzir os erros e crimes cometidos nas vias e estradas.</p>
             </div>
         </div>
     </div>

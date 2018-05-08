eaf5f0be5ccf069a4f56f2886ec60420da8df3f7
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-08 17:48:49 -0300

    "mais que transito" desktop

--- page-mais-que-transito.php
+++ page-mais-que-transito.php
@@ -6,46 +6,65 @@
             </div>
         
             <div class="hero text-light p-4" style="background:black">
-                <img src="img/" alt="Mais que trânsito">
-                <p >Lorem ipsum dolor, sit amet consectetur adipisicing elit. In explicabo hic odio vero commodi unde, repellat facere alias cumque voluptatum necessitatibus iure corporis rem dolorem. Rerum commodi vel vitae voluptatum?
-                Quae, obcaecati enim nostrum voluptatem quia dolores optio, consequatur, itaque natus quos voluptates? Voluptates, ex amet, quasi veritatis distinctio quas accusamus necessitatibus eius dolores nobis veniam, tenetur laboriosam non ut.
-                Aliquid omnis nostrum esse nulla dolorem optio deserunt laborum consectetur quasi quam doloremque neque, porro aliquam repudiandae harum a voluptatem cum aspernatur! Quis, itaque odit dolore molestiae perspiciatis modi eos!</p>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="img/" alt="Mais que trânsito">
+                        </div>
+                        <div class="col-md-6">
+                            <p >Lorem ipsum dolor, sit amet consectetur adipisicing elit. In explicabo hic odio vero commodi unde, repellat facere alias cumque voluptatum necessitatibus iure corporis rem dolorem. Rerum commodi vel vitae voluptatum?
+                            Quae, obcaecati enim nostrum voluptatem quia dolores optio, consequatur, itaque natus quos voluptates? Voluptates, ex amet, quasi veritatis distinctio quas accusamus necessitatibus eius dolores nobis veniam, tenetur laboriosam non ut.
+                            Aliquid omnis nostrum esse nulla dolorem optio deserunt laborum consectetur quasi quam doloremque neque, porro aliquam repudiandae harum a voluptatem cum aspernatur! Quis, itaque odit dolore molestiae perspiciatis modi eos!</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
             
         
             <!------------------ CONTENT -------------------->
-            <div>
-                <div class="container">
-                    <h2 class="bebas-title">Antes da lei seca</h2>
-                    <ul>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    </ul>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="container">
+                        <h2 class="bebas-title">Antes da lei seca</h2>
+                        <ul>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        
-            <div class="depois-da-lei-seca">
-                <div class="container">
-                    <h2 class="bebas-title">Depois da lei seca</h2>
-                    <ul>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    </ul>
-                </div>
-            </div>
-        
-            <?php get_template_pat("quots");?>
             
-            <div class="container">
-                <h2 class="bebas-title">Ajude a salvar vidas</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius expedita similique aspernatur deleniti. Facilis omnis, recusandae sunt vero ipsam officia aspernatur possimus totam dolore a vel numquam modi veritatis nulla!</p>
-                <img src="img/" alt="Seja um fiscal da vida. Tire seu certificado">
+                <div class="depois-da-lei-seca col-md-6">
+                    <div class="container">
+                        <h2 class="bebas-title">Depois da lei seca</h2>
+                        <ul>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        
+            <?php get_template_part("quots");?>
+            
+            <div class="container clearfix pb-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="bebas-title text-left">Ajude a salvar vidas</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius expedita similique aspernatur deleniti. Facilis omnis, recusandae sunt vero ipsam officia aspernatur possimus totam dolore a vel numquam modi veritatis nulla!</p>
+                        <img class="d-md-none" src="img/" alt="Seja um fiscal da vida">
+                        <a href="#" class="blog-button hover">Tire seu certificado</a>
+                    </div>
+                    <div class="col-md-6 d-none d-md-block">
+                        <img src="img/" alt="Seja um fiscal da vida">
+                    </div>
+                </div>
             </div>
         
                     

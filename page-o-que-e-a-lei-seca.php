2931e3a1d80b0f8261c75f9c4bfb6ae768be3ffb
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-16 01:50:31 -0300

    Resolvido, problema com imagens expremidas

--- page-o-que-e-a-lei-seca.php
+++ page-o-que-e-a-lei-seca.php
@@ -23,46 +23,46 @@
     <!------------------ CONTENT -------------------->
     <div id="lei-seca" class="lei-seca-content container">
         <div class="row">   
-            <div class="col-md-6 order-md-2 position-relative">
+            <div class="col-lg-6 order-lg-2 position-relative">
                 <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/leiseca-img1.jpg'?>">
-                <img class="year d-md-none position-absolute" src="<?php echo get_template_directory_uri() . '/img/2009.png'?>" alt="">
+                <img class="year d-lg-none position-absolute" src="<?php echo get_template_directory_uri() . '/img/2009.png'?>" alt="">
             </div>
-            <div class="col-md-6 p-md-5 position-relative">
-                <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2009-2.png'?>">
+            <div class="col-lg-6 p-md-5 position-relative">
+                <img class="year-2 position-absolute d-none d-lg-block" src="<?php echo get_template_directory_uri() . '/img/2009-2.png'?>">
                 <h2 class="text-uppercase text-center">A primeira operação Lei Seca</h2>
                 <p>A primeira Operação Lei Seca ocorreu em 19 de março de 2009, e, no seu início, 20% dos motoristas registravam a presença de bebida alcoólica no sangue. Em 2016, o número de motoristas pegos com traços de bebida alcoólica no sangue caiu para 7%. Já em seu primeiro mês de funcionamento, a lei mostrou que faz a diferença. Segundo dados do Ministério da Saúde, ocorreu no período uma diminuição de 24% de resgates do SAMU. Em um ano, o número de mortes em acidentes causados por motoristas embriagados caiu em 32% em comparação com o ano anterior.</p>
             </div>
         </div>
         <div class="row">
-        <div class="col-md-6 position-relative">
+        <div class="col-lg-6 position-relative">
             <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/leiseca-img2.jpg'?>">
-            <img class="year position-absolute d-md-none" src="<?php echo get_template_directory_uri() . '/img/2012.png'?>" alt="">
+            <img class="year position-absolute d-lg-none" src="<?php echo get_template_directory_uri() . '/img/2012.png'?>" alt="">
         </div>
-            <div class="col-md-6">
-                <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2012-2.png'?>">
+            <div class="col-lg-6">
+                <img class="year-2 position-absolute d-none d-lg-block" src="<?php echo get_template_directory_uri() . '/img/2012-2.png'?>">
                 <h2 class="text-uppercase text-center">Modificações da Lei Seca</h2>
                 <p>A lei foi modificada em 2012, para endurecer ainda mais a fiscalização no trânsito. Inicialmente a prova da embriaguez só poderia ser feita através do teste do bafômetro ou exame do sangue do motorista em laboratório. Após a mudança, passaram a valer como provas o depoimento do policial, testemunhos de terceiros, fotos e vídeos, além de um aumento considerável no valor da multa para o condutor que for flagrado sob influência do álcool. Com essas modificações diminuíram ainda mais as ocorrências de motoristas alcoolizados e acidentes causados pela combinação de álcool e direção. </p>
             </div>
             
         </div>
         <div class="row">
-            <div class="col-md-6 order-md-2 position-relative">
+            <div class="col-lg-6 order-lg-2 position-relative">
                 <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/leiseca-img3.jpg'?>">
-                <img class="year position-absolute d-md-none" src="<?php echo get_template_directory_uri() . '/img/2016.png'?>" alt="">
+                <img class="year position-absolute d-lg-none" src="<?php echo get_template_directory_uri() . '/img/2016.png'?>" alt="">
             </div>
-            <div class="col-md-6">
-                <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2016-2.png'?>">
+            <div class="col-lg-6">
+                <img class="year-2 position-absolute d-none d-lg-block" src="<?php echo get_template_directory_uri() . '/img/2016-2.png'?>">
                 <h2 class="text-uppercase text-center">As modificações</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sint voluptatibus reprehenderit! Obcaecati, facilis fugit animi necessitatibus nostrum officiis architecto magnam ducimus impedit optio eveniet tempore quaerat eum cupiditate cumque!Ex ad est recusandae laudantium natus, repellendus placeat atque quis sint accusantium porro, dignissimos fuga voluptates aliquam in, sit distinctio id sed. Dolorem unde culpa ipsum ab neque earum quam.</p>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 position-relative">
+            <div class="col-lg-6 position-relative">
                 <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/leiseca-img4.jpg'?>">
-                <img class="year position-absolute d-md-none" src="<?php echo get_template_directory_uri() . '/img/2017.png'?>" alt="">
+                <img class="year position-absolute d-lg-none" src="<?php echo get_template_directory_uri() . '/img/2017.png'?>" alt="">
             </div>
-            <div class="col-md-6">
-                <img class="year-2 position-absolute d-none d-md-block" src="<?php echo get_template_directory_uri() . '/img/2017-2.png'?>">
+            <div class="col-lg-6">
+                <img class="year-2 position-absolute d-none d-lg-block" src="<?php echo get_template_directory_uri() . '/img/2017-2.png'?>">
                 <h2 class="text-uppercase text-center">Trânsito mais humanizado</h2>
                 <p>Em 2016, novas modificações foram feitas para aprimorar a Lei Seca e o valor da multa aplicada ao motorista que dirige embriagado aumentou para 10 no fator de multiplicação: De R$1.912,00 para R$2.934,70, além de ter habilitação suspensa pelo período de 12 meses. A intenção é que o trânsito seja humanizado, com a contribuição e união de todos. A reeducação é necessária e está sendo feita através das penalizações para reduzir os erros e crimes cometidos nas vias e estradas.</p>
             </div>

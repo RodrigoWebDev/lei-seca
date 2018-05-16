4afec2926fb92122c08b0aa7488ebffe827607ae
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-16 03:05:27 -0300

    Adicionado links das redes sociais

--- header.php
+++ header.php
@@ -15,8 +15,15 @@
     <header>
         <div class="container">
             <div class="row">
-                <div class="col-3 col-md-6 d-flex align-items-center"><a class="hover" href="#"><i class="ion-email"></i><span class="d-none d-md-inline">contato@leiseca.com.br</span></a></div>
-                <div class="col-9 col-md-6 text-right social"><a class="hover" href="#"><i class="ion-social-facebook"></i></a><a class="hover" href="#"><i class="ion-social-instagram"></i></a><a class="hover" href="#"><i class="ion-social-twitter"></i></a><a class="hover" href="#"><i class="ion-search"></i></a></div>
+                <div class="col-3 col-md-6 d-flex align-items-center">
+                    <a class="hover" href="#" target="_blank"><i class="ion-email"></i><span class="d-none d-md-inline">contato@leiseca.com.br</span></a>
+                </div>
+                <div class="col-9 col-md-6 text-right social">
+                    <a class="hover" href="https://www.facebook.com/leisecasalvavidas/" target="_blank"><i class="ion-social-facebook"></i></a>
+                    <a class="hover" href="https://www.instagram.com/leisecasalva/" target="_blank"><i class="ion-social-instagram"></i></a>
+                    <a class="hover" href="https://twitter.com/leisecasalva" target="_blank"><i class="ion-social-twitter"></i></a>
+                    <a class="hover" href="#" target="_blank"><i class="ion-search"></i></a>
+                </div>
             </div>
         </div>
     </header>

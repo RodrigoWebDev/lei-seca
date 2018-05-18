0ac9227d0332f14abf4e335e74850488c473d4fc
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-17 21:12:37 -0300

    Ajustes

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@
 	function load_scripts(){
 		wp_enqueue_style("style", get_template_directory_uri() . "/css/style.css");
         wp_enqueue_style("icons", get_template_directory_uri() . "/css/ionicons.min.css");
+        wp_enqueue_script("popper-js", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",null, null, true);
+        wp_enqueue_script("js-bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",null, null, true);
         wp_enqueue_script("script", get_template_directory_uri() . "/js/script.js", array("jquery"), null, true);
-        wp_enqueue_script("popper-js", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js", null, true);
-        wp_enqueue_script("js-bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js", null, true);
 	}
 
 	add_action("wp_enqueue_scripts", "load_scripts");
--- page-fale-conosco.php
+++ page-fale-conosco.php
@@ -1,17 +1,17 @@
 <?php get_header();?>
-    <div class="fale-conosco">
+<div class="fale-conosco">
     <div class="contact py-5">
         <div class="container">
             <div class="row">
                 <div class="col-md-5">
                     <h1 class="bebas-title text-center text-md-left">Fale conosco</h1>
-                    <p class="text-center text-md-left">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus harum tenetur veritatis perspiciatis.</p>
+                    <p class="text-center text-md-left">Quer nos ajudar? Tem alguma dúvida, opinião ou sugestão? Estamos aqui para ouvir você! Entre em contato.</p>
                     <div class="social">
                         <div>
-                            <a class="hover" href="#"><i class="ion-social-facebook"></i><span style="font-size:20px;" class="d-none d-md-inline"> /leisecasalvavidas</span></a>
+                            <a class="hover" href="https://www.facebook.com/leisecasalvavidas/" target="_blank"><i class="ion-social-facebook"></i><span style="font-size:20px;" class="d-none d-md-inline"> /leisecasalvavidas</span></a>
                         </div>
-                        <a class="hover" href="#"><i class="ion-social-instagram"></i><span style="font-size:20px;" class="d-none d-md-inline"> @leisecasalva</span></a>
-                        <a class="hover" href="#"><i class="ion-social-twitter"></i><span style="font-size:20px;" class="d-none d-md-inline"> @leisecasalva</span></a>
+                        <a class="hover" href="https://www.instagram.com/leisecasalva/" target="_blank"><i class="ion-social-instagram"></i><span style="font-size:20px;" class="d-none d-md-inline"> @leisecasalva</span></a>
+                        <a class="hover" href="https://twitter.com/leisecasalva" target="_blank"><i class="ion-social-twitter"></i><span style="font-size:20px;" class="d-none d-md-inline"> @leisecasalva</span></a>
                     </div>
                     <hr class="d-md-none">
                 </div>
@@ -19,7 +19,7 @@
                 <div class="col-md-2 d-flex align-items-center">
                     <div class="vertical-line d-none d-md-block"></div>
                 </div>
-                
+
                 <div class="col-md-5">
                     <form>
                         <div class="row">
@@ -27,7 +27,10 @@
                             <div class="col-md-12"><input class="w-100" type="email" name="email" placeholder="Email"></div>
                         </div>
                         <div class="row">
-                            <div class="col-md-12 pt-4"><textarea class="w-100" placeholder="Deixe sua mensagem"></textarea><input class="button float-right my-4 hover" type="submit" value="Enviar"></div>
+                            <div class="col-md-12 pt-4">
+                                <textarea class="w-100" placeholder="Deixe sua mensagem"></textarea>
+                                <input class="button float-right my-4 hover" type="submit" value="Enviar">
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -35,10 +38,7 @@
         </div>
     </div>
 
-    </div>
-        
-                    
-    <?php get_footer(); ?>
+</div>
 
-    
-    
+
+<?php get_footer(); ?>

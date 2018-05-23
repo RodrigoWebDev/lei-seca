21708d21dac2ef625e146e8717d023fddd3e4d5d
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-23 18:10:52 -0300

    Novas imagens no slider da Home

--- page-home.php
+++ page-home.php
@@ -7,17 +7,13 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-1.png' ?>" alt="First slide">
+            <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-2.png' ?>" alt="First slide">
             </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-2.png' ?>" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-3.png' ?>" alt="Third slide">
+            <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-3.png' ?>" alt="Second slide">
             </div>
             <div class="carousel-item">
             <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/banner-4.png' ?>" alt="Third slide">

331fab33cc723e075ad9e60d7b05985ff80a041f
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-23 18:21:37 -0300

    Ajuste nas imagens da pagina single

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <head>
-    <title>Lei seca</title>
+    <title><?php bloginfo('name'); ?>  <?php wp_title(); ?>  </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">

e582aeece0ffce547c6bcd950a10e7f4fae4d56d
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-05-26 13:23:49 -0300

    Texto da pagina mais que transito

--- page-mais-que-transito.php
+++ page-mais-que-transito.php
@@ -8,8 +8,12 @@
                 <div class="col-md-6">
                     <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/placa.png' ?>" alt="Mais que transito">
                 </div>
-                <div class="col-md-6 d-flex align-items-center">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In explicabo hic odio vero commodi unde, repellat facere alias cumque voluptatum necessitatibus iure corporis rem dolorem. Rerum commodi vel vitae voluptatum? Quae, obcaecati enim nostrum voluptatem quia dolores optio, consequatur, itaque natus quos voluptates? Voluptates, ex amet, quasi veritatis distinctio quas accusamus necessitatibus eius dolores nobis veniam, tenetur laboriosam non ut. Aliquid omnis nostrum esse nulla dolorem optio deserunt laborum consectetur quasi quam doloremque neque, porro aliquam repudiandae harum a voluptatem cum aspernatur! Quis, itaque odit dolore molestiae perspiciatis modi eos!</p>
+                <div style="flex-direction:column" class="col-md-6 d-flex justify-content-center">
+                    <p>Muitos cidadãos ainda assimilam a Lei Seca à realização de blitzes, trânsito e multas, mas é muito além disso. Você sabia que a Lei Seca salvou mais de 41 mil vidas nos 10 anos de execução? A iniciativa não tem como prioridade punir os motoristas, mas conscientizá-los para que vidas sejam poupadas.</p>
+
+                    <p>Diante de um acidente de trânsito, o governo gasta recursos para os prejuízos e para indenizar as famílias de possíveis vítimas e ao evitar que tais acidentes ocorram, é claro que haverá economia. Mas esse não é o ponto mais importante. O verdadeiro prejuízo pode ser ao seu bem estar e à felicidade da sua família ao perder um ente querido.</p>
+
+                    <p>A Lei Seca está em vigor há anos para colher resultados que zelem pela vida da população, reeducar condutores sobre a direção defensiva, pensando neles, em quem está no carro e em quem passará pelo seu caminho. Ao decorrer dos anos, temos o orgulho de exibir os seguintes números e sempre desejando melhorar cada vez mais:</p>
                 </div>
             </div>
         </div>
@@ -18,29 +22,72 @@
 
 
     <!------------------ CONTENT -------------------->
-    <div class="row">
+    <h2 style="font-size:50px;" class="bebas-title">Reflexos da Lei Seca:</h2>
+    <div class="row reflexos">
         <div class="col-md-6">
             <div class="container">
-                <h2 class="bebas-title">Antes da lei seca</h2>
                 <ul>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                    <li>Fim de ano 2017/2018</li>
+                    <div>
+                        <p>9.360 motoristas abordados</p>
+                        <p>1.630 multas aplicadas</p>
+                        <p>521 carteiras de habilitação recolhidas</p>
+                        <p>272 veículos rebocados</p>
+                    </div>
+                    <li>Carnaval 2018</li>
+                    <div>
+                        <p>6.134 motoristas abordados</p>
+                        <p>499 carteiras de habilitação recolhidas</p>
+                        <p>Fim de ano 2016/2017</p>
+                        <p>7.661 motoristas abordados</p>
+                        <p>1.694 multas aplicadas</p>
+                        <p>590 carteiras de habilitação recolhidas</p>
+                        <p>228 veículos rebocados</p>
+                    </div>
+                    <li>Carnaval 2017</li>
+                    <div>
+                        <p>5.472 motoristas abordados</p>
+                        <p>1.486 multas aplicadas</p>
+                        <p>415 carteiras de habilitação recolhidas</p>
+                        <p>246 veículos rebocados</p>
+                    </div>
+                    <li>De 2009 a 2016</li>
+                    <p>-43% motoristas flagrados dirigindo sob efeito do álcool De 2009 a 2015</p>
                 </ul>
             </div>
         </div>
 
         <div class="depois-da-lei-seca col-md-6">
             <div class="container">
-                <h2 class="bebas-title">Depois da lei seca</h2>
                 <ul>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
-                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente modi ea quam obcaecati perferendis tempora animi soluta quia, vitae officiis eius magnam eveniet illum, laborum dolore! Quis id at qui.</li>
+                    <li>De 2009 a 2016</li>
+                    <div>
+                        <p>-28% mortos no trânsito a cada 100 mil habitantes Brasil</p>
+                    </div>
+
+                    <li>Brasil</li>
+                    <div>
+                        <p>4º País com mais mortes no trânsito no mundo</p>
+                    </div>
+                    <li>Lei Seca 9 anos</li>
+                    <div>
+                        <p>90% de aprovação da população</p>
+                        <p>2.000 palestras realizadas em escolas, universidades e empresas</p>
+                        <p>521.346 multas aplicadas</p>
+                        <p>100.974 veículos rebocados</p>
+                        <p>183.219 motoristas flagrados dirigindo sob efeito do álcool</p>
+                        <p>174.509 carteiras de habilitação recolhidas</p>
+                        <p>20.295 blitzes realizadas</p>
+                        <p>2,8 milhões de motoristas fiscalizados</p>
+                    </div>
+
+                    <li>7,9% dos motoristas flagrados alcoolizados em 2009</li>
+                    <div> </div>
+                    <li>4,3% dos motoristas flagrados alcoolizados em 2017</li>
+                    <div> </div>
+                    <li>Valor da multa para quem dirigir alcoolizado R$ 2.934,70</li>
+                    <div>
+                    </div>
                 </ul>
             </div>
         </div>
